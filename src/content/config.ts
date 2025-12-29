a7bc76fa134dd37eaa2853284f7c05f634f692d1
Phạm Thiện <phamthienx@gmail.com>
2025-12-29 12:57:29 +0000

    s

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,8 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
 	schema: z.object({
 		title: z.string(),
-		published: z.date(),
-		updated: z.date().optional(),
+		published: z.string(),
+		updated: z.string().optional(),
 		draft: z.boolean().optional().default(false),
 		description: z.string().optional().default(""),
 		image: z.string().optional().default(""),
